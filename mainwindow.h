5883b62a782c141a453e321ab6748f7968f84b77
Alexander Tyuryuchkin <tyuryuchkin@gmail.com>
2019-11-26 22:33:06 +0300

    feat Добавил валидатор для ip ввода

---
+++ mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTcpSocket>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButtonConnect_clicked();
+    void connectedToHost();
+    void disconnectedFromHost();
+    void readFromHost();
+
+    void on_pushButtonSend_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QTcpSocket socket;
+};
+#endif // MAINWINDOW_H
